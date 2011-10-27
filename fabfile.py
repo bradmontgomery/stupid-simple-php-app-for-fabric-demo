912ca8f37a4dee69b3bd329f782a621c7fda85f4
Brad Montgomery <brad@bradmontgomery.net>
2011-10-27 00:57:17 -0500

    added task to install linux packages

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,9 @@ from fabric.api import run
 def host_type():
     run('uname -s')
 
+def install_packages():
+    """ Install a bare minimum LAMP stack """
+    run('apt-get install aptitude')
+    run('aptitude update')
+    run('aptitude upgrade')
+    run('aptitude install libapache2-mod-php5 php5 php-pgsql git')
