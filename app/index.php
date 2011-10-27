119dba3617d0ea9684bd351331479b7e5196d390
Brad Montgomery <brad@bradmontgomery.net>
2011-10-27 16:59:41 -0500

    made teh app database-driven

--- app/index.php
+++ app/index.php
@@ -1,11 +1,24 @@
+<?php
+error_reporting  (E_ALL);
+ini_set('display_errors', true);
+?>
 <!doctype html>
 <html>
 <head><title>hello world</title></head>
 <body>
-<h1>
-<?php 
-    echo 'Hello World!';
+<h1><?php echo 'Hello World!'; ?></h1>
+<hr/>
+<?php
+    $conn = pg_connect("host=localhost dbname=appdb user=dbuser");
+    $result = pg_query("SELECT * FROM things");
+    $rows = pg_fetch_all($result);
+    if($rows) {
+        echo '<ul>';
+        foreach($rows as $item) {
+            echo '<li>' . $item['name'] . '</li>';
+        }
+        echo '</ul>';
+    }
 ?>
-</h1>
 </body>
 </html>
