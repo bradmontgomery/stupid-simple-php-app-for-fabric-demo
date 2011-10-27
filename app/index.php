df8bbcc75a894de75fbe368303b5b35a51c110db
Brad Montgomery <brad@bradmontgomery.net>
2011-10-26 23:57:18 -0500

    added app folder and initial fabfile example

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head><title>hello world</title></head>
+<body>
+<h1>
+<?php 
+    echo 'Hello World!';
+?>
+</h1>
+</body>
+</html>
---
+++ fabfile.py
@@ -0,0 +1,5 @@
+from fabric.api import run
+
+def host_type():
+    run('uname -s')
+
